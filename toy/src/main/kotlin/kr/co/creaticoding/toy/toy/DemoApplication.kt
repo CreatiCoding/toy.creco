69590433e65dcd536b02b002c2b15ec4a1c06d09
xreco <xreaticoding@gmail.com>
2019-06-26 23:17:13 +0900

    init my web site

---
+++ toy/src/main/kotlin/kr/co/creaticoding/toy/toy/DemoApplication.kt
@@ -0,0 +1,13 @@
+package kr.co.creaticoding.toy.toy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.core.env.Environment
+import org.springframework.boot.runApplication
+import java.net.InetAddress
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
